f8551b40614fdc22ec2fc092aae82fc7805b3a91
HAEUN SON <ha3unson@gmail.com>
2022-12-22 13:42:36 -0800

    Title Menu Scene

---
+++ Assets/Scripts/TitleMenuManager/TitleMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMenu : MonoBehaviour
+{
+    //public void OnClickNewGame()
+  // {
+    //    GameStateManager.NewGame(); //Call new game to start the game.
+   // }
+
+   // public void OnClickQuitGame()
+  //  {
+   //     Application.Quit(); // Quit the game in the build.
+  //  }
+}
